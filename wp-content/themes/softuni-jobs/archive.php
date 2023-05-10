ddeb36c525b8062fc65addee105d48d4b5dcde23
CvetiTo <email@example.com>
2023-05-10 17:12:46 +0300

    Theme: add template part, adjust the header, index

--- wp-content/themes/softuni-jobs/archive.php
+++ wp-content/themes/softuni-jobs/archive.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 
+<?php the_archive_title(); ?>
+
 <ul class="jobs-listing">
 	<?php if (have_posts()) : ?>
 
 		<?php while (have_posts()) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/job', 'item'); ?> 
+			<?php get_template_part( 'template-parts/post', 'item'); ?> 
 
 		<?php endwhile; ?>
 
---
+++ wp-content/themes/softuni-jobs/template-parts/post-item.php
@@ -0,0 +1,30 @@
+<li class="job-card">
+    <div class="job-primary">
+        <h2 class="job-title">
+            <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?>
+            </a>
+        </h2>
+        <div class="job-meta">
+            <a class="meta-company">
+                <?php echo the_category();  
+                //http://localhost/softuni-jobs-exam/?category_name=
+                //http://localhost/softuni-jobs-exam/?cat=2 sorted category
+                //http://localhost/softuni-jobs-exam/?author=1 sorted author-admin
+                 ?>
+        </a>
+            <span class="meta-date"><?php the_date(); ?></span>
+        </div>
+    </div>
+    <div class="job-logo">
+        <div class="job-logo-box">
+            <?php
+            if (has_post_thumbnail()) {
+                the_post_thumbnail();
+            } else {
+                echo '<img src="https://i.imgur.com/ZbILm3F.png" alt="default image thumbnail">';
+            }
+            ?>
+        </div>
+    </div>
+</li>
